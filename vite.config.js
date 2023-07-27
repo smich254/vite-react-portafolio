e809d323e5471d85126eaa4275cdacbaa87a7769
smic254 <smich254@gmail.com>
2023-07-27 09:51:39 -0500

    Actualizando vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://smich254.github.io/vite-react-portafolio/'
 })
